741cd09c07b4e816b78117e44aae9d73345ff22c
niyati-dhanani <niyatidhanani05@gmail.com>
2025-08-19 18:20:10 +0530

    blinkit

--- pre_12/src/firebaseConfig.js
+++ pre_12/src/firebaseConfig.js
@@ -1,17 +1,31 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD74t68x44URYtbEWld9RUrmB3LY9j3kzU",
-  authDomain: "react-5pm-f05f3.firebaseapp.com",
-  projectId: "react-5pm-f05f3",
-  storageBucket: "react-5pm-f05f3.firebasestorage.app",
-  messagingSenderId: "486764440315",
-  appId: "1:486764440315:web:783abf97fd4c124d82de5e"
+  apiKey: "AIzaSyDH5h484mqs3xmKwBUzvgSgYQXJTAQzP-M",
+  authDomain: "fir-crud-2fbb5.firebaseapp.com",
+  projectId: "fir-crud-2fbb5",
+  storageBucket: "fir-crud-2fbb5.firebasestorage.app",
+  messagingSenderId: "153938342738",
+  appId: "1:153938342738:web:ecb13209cb7c58eff8c08b",
+  measurementId: "G-CF4MXHCDSF"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+
+
+
+
+
+
+
+
